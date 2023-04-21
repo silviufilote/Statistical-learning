d95d9b8a06ae94fa1d2ef688400bf645ed68bba8
silviufilote <fsilviu89@gmail.com>
2023-04-21 15:16:44 +0200

    update

--- trees.R
+++ trees.R
@@ -29,7 +29,16 @@ library(performance)
 library(lmtest)
 
 
-# Outliers detaction
+# importing the dataset
+data <- read.csv("C:\\Users\\fsilv\\Dropbox\\UNI\\stistics\\1.csv")
+data <- data.frame(data[,2:9])
+colnames(data)[1] = "GRE"
+colnames(data)[2] = "TOEFL"
+colnames(data)[3] = "UniRatings"
+colnames(data)[8] = "Admit"
+
+
+# removing outliers
 detect_outlier <- function(x) {
   Quantile1 <- quantile(x, probs=.25)
   Quantile3 <- quantile(x, probs=.75)
@@ -38,7 +47,8 @@ detect_outlier <- function(x) {
 }
 
 
-remove_outlier <- function(dataframe,columns=names(dataframe)) {
+remove_outlier <- function(dataframe) {
+  columns <- names(dataframe)
   for (col in columns) {
     dataframe <- dataframe[!detect_outlier(dataframe[[col]]), ]
   }
@@ -46,17 +56,11 @@ remove_outlier <- function(dataframe,columns=names(dataframe)) {
   print(dataframe)
 }
 
+for(i in 1:2){
+  data <- remove_outlier(data)
+}
 
-# Load dataset and setup
-data <- read.csv("C:\\Users\\fsilv\\Dropbox\\UNI\\stistics\\1.csv")
-data <- data.frame(data[,2:9])
-colnames(data)[1] = "GRE"
-colnames(data)[2] = "TOEFL"
-colnames(data)[3] = "UniRatings"
-colnames(data)[8] = "Admit"
-
-data <- remove_outlier(data, c('GRE', 'TOEFL', 'UniRatings', 'SOP', "LOR", 'CGPA', "Research"))
-data <- data %>% relocate(CGPA, .after=TOEFL)
+# generating the dummy variables
 data <- data %>% 
   mutate_at(vars(Research, LOR, SOP, UniRatings), as.factor)
 
@@ -73,39 +77,58 @@ train = sample(dim(data)[1], dim(data)[1]*0.7, replace = FALSE)
 install.packages("tree")
 library(tree)
 
-tree_model <- tree( Admit ~ . , data, split = "gini")
+tree_model <- tree( Admit ~ . , data, subset = train, split = "gini")
 summary(tree_model)
 plot(tree_model)
 text(tree_model,pretty = 0)
 
 
+# cross-validation for pruning
 model_tree_cv <- cv.tree(tree_model,  FUN = prune.tree)
-
-# dev = cross-validation error rate
-# size = number of terminal nodes of each tree -> complexity
-plot(model_tree_cv$size, model_tree_cv$dev, ylab = "cross-validation error rate", xlab = "Complexity = foglie")
-
 best = min(model_tree_cv$size[model_tree_cv$dev == min(model_tree_cv$dev)])
 prune <- prune.tree(tree_model, best = best)
 summary(prune)
-plot(prune)
-text(prune, pretty = 0)
 
+
+# plotting tree
+par(mfrow = c(1,2))
+# dev = cross-validation error rate
+# size = number of terminal nodes of each tree -> complexity
+plot(model_tree_cv$size, model_tree_cv$dev, ylab = "cross-validation total rate", xlab = "Complexity = foglie", main ="Cross-validation total leaves")
+abline(v = 8, col = "blue", lty = "dashed")
+text(8, 15, "Benchmark", adj = c(0.5, -0.5))
+plot(prune, main="Main title",
+     xlab="X axis title",
+     ylab="Y axis title",
+     sub="Sub-title")
+text(prune, pretty = 0, main = "Tree pruned")
 
 
 # validation and MSE
-tree_model_train <- tree( Admit ~ . , data, subset = train, split = "gini")
-train_prune <- prune.tree(tree_model_train, best = best)
-summary(tree_model_train)
-
-pred_value <- predict(tree_model_train, newdata = data[-train,])
-# table(pred_value, data$Admit[-train])
+yhat <- predict(tree_model, newdata = data[-train,])
+res = data$Admit[-train] - yhat
+test_RMSE_tree = round(sqrt(mean((res)^2)), digits = 3)
 
 
-res = data$Admit[-train] - pred_value
-test_MSE_tree = mean((res)^2)
-bptest(tree_model_train) # non sono omoschedastici
+
+# residual analysis
+bptest(tree_model) # non sono omoschedastici
 shapiro.test(res) # non sono normali
+
+par(mfrow = c(1,4))
+hist(res,40,
+     xlab = "Residual",
+     main = "Distribuzione empirica dei residui") 
+
+plot(res, pch = "o", col = "blue" ,
+     ylab = "Residual", main = paste0("Residual plot - mean:",round(mean(res),digits = 4),
+                                      "- var:", round(var(res),digits = 4)))
+abline(c(0,0),c(0,length(res)), col= "red", lwd = 2)
+
+boxplot(res, ylab = "Residuals", main = "Outliers")$out
+
+qqnorm(res, main='Residuals')
+qqline(res)
 
 
 ################################################################################
@@ -121,165 +144,230 @@ bagg_test <- randomForest(Admit ~ . ,data = data, subset = train,
                            mtry = ncol(data)-1, importance = TRUE, replace = TRUE)
 bagg_test
 plot(bagg_test, main = "Bagged Trees: Error vs Number of Trees")
-grid()
+abline(v = 100, col = "blue", lty = "dashed")
 
-# utilizzo 500 alberi
-bagg_test$ntree
-
-# validation
-yhat <- predict(bagg_test, newdata = data[-train,])
-test_MSE_bagging <- mean((yhat - data$Admit[-train])^2)
-
-bptest(bagg_model) # non sono omoschedastici
-shapiro.test((yhat - data$Admit[-train])) # non sono normali 
-importance(bagg_test)
 
 
 # fare crossvalidazione sul numero di alberi
-mse_cv_bag = rep(0,100)
-for (i in 1:100){
+rmse_cv_bag = rep(0,10)
+for (i in 1:10){
   bagg_model_cv <- randomForest(Admit ~ . ,data = data, subset = train,
                                 mtry = ncol(data)-1, importance = TRUE, replace = TRUE, ntree = (i * 10))
   yhat_ite <- predict(bagg_model_cv, newdata = data[-train,])
-  mse_cv_bag[i] = mean((yhat_ite - data$Admit[-train])^2)
+  rmse_cv_bag[i] = sqrt(mean((yhat_ite - data$Admit[-train])^2))
 }
 
-treesX = c(1:100)*10
-plot(treesX, mse_cv_bag, type = 'b')
-grid()
 
+# pick the best number of trees
+nTrees = c(1:10)*10
+plot(nTrees, rmse_cv_bag, ylab="test RMSE", xlab="n Trees", main="Choosing the best number of trees", type = 'b')
 
-nTrees_bagg <- 0
-for(i in 1:100){
-  if(mse_cv_bag[i] == min(mse_cv_bag)){
-    nTrees_bagg <- i * 10
+for(i in 1:10){
+  if(rmse_cv_bag[i] == min(rmse_cv_bag)){
+    abline(v = i * 10, col = "blue", lty = "dashed")
   }
 }
+
+nTrees_min_bagg <- 0
+for(i in 1:10){
+  if(rmse_cv_bag[i] == min(rmse_cv_bag)){
+    nTrees_min_bagg <- i * 10
+  }
+}
+
+
+# validation
+bagg_model <- randomForest(Admit ~ . ,data = data, subset = train,
+                              mtry = ncol(data)-1, importance = TRUE, replace = TRUE, ntree = nTrees_min_bagg)
+yhat <- predict(bagg_model, newdata = data[-train,])
+res_bagg = yhat - data$Admit[-train]
+test_RMSE_bagging <- sqrt(mean((res_bagg)^2))
+importance(bagg_model)
+bagg_model
+
+
+
+# residuals
+bptest(bagg_model) 
+shapiro.test(res_bagg) 
+par(mfrow = c(1,4))
+hist(res_bagg,40,
+     xlab = "Residual",
+     main = "Distribuzione empirica dei residui") 
+
+plot(res_bagg, pch = "o", col = "blue" ,
+     ylab = "Residual", main = paste0("Residual plot - mean:",round(mean(res_bagg),digits = 4),
+                                      "- var:", round(var(res_bagg),digits = 4)))
+abline(c(0,0),c(0,length(res_bagg)), col= "red", lwd = 2)
+
+boxplot(res_bagg, ylab = "Residuals", main = "Outliers")$out
+
+qqnorm(res_bagg, main='Residuals')
+qqline(res_bagg)
+
 
 ################################################################################
 #########################################  Random forest
 ################################################################################
 
+library(randomForest)
 
-forest_model <- randomForest(Admit ~ . ,data = data, subset = train,
-                             mtry = floor(sqrt(ncol(data)-1)), importance = TRUE)
-
-
-plot(forest_model,type = 'b',col="green",pch = "+")
-par(new=TRUE)
-plot(bagg_test,type = 'b',col="red",pch='o')
-
-yhat <- predict(forest_model, newdata = data[-train,])
-test_MSE_forest <- mean((yhat - data$Admit[-train])^2)
-
-forest_model
-importance(forest_model)
-bptest(forest_model) 
-shapiro.test((yhat - data$Admit[-train])) # non sono normali 
+set.seed(1)
 
 # Cross validation n trees and selection predictios
-
-mse_cv_forest <- matrix(nrow=6, ncol=100)
+forest_cv_models <- list()
 for(x in 1:6){
-  for (i in 1:100){
-    forest_model_ite <- randomForest(Admit ~ . ,data = data, subset = train,
-                                    mtry = x, importance = TRUE, replace = TRUE, ntree = (i * 10))
-    
-    forest_cv_models[[x,i]] <- forest_model_ite
-    yhat_ite <- predict(forest_model_ite, newdata = data[-train,])
-    mse_cv_forest[[x,i]] = mean((yhat_ite - data$Admit[-train])^2)
-  }
+  forest_model_ite <- randomForest(Admit ~ . ,data = data, subset = train,
+                                   mtry = x, importance = TRUE, replace = TRUE, ntree = 150)
+  forest_cv_models <- append(forest_cv_models, list(forest_model_ite))
 }
 
-par(mfrow = c(3,2))
-plot(treesX, mse_cv_forest[1,], type = 'b')
-plot(treesX, mse_cv_forest[2,], type = 'b')
-plot(treesX, mse_cv_forest[3,], type = 'b')
-plot(treesX, mse_cv_forest[4,], type = 'b')
-plot(treesX, mse_cv_forest[5,], type = 'b')
-plot(treesX, mse_cv_forest[6,], type = 'b')
 
-
-nTrees_forest <- matrix(nrow=2, ncol=6)
-
+# create matrix with nReg per split, RMSE min, nTrees
+rmse_min_reg <- matrix(list(), nrow=6, ncol=3)
 for(x in 1:6){
-  for(i in 1:100){
-    if(mse_cv_forest[i] == min(mse_cv_forest[x,])){
-      nTrees_forest[1,x] <- (i * 10)
-      nTrees_forest[2,x] <- mse_cv_forest[i]
+  for(i in 1:150){
+    if(forest_cv_models[[x]]$mse[i] == min(forest_cv_models[[x]]$mse)){
+      rmse_min_reg[[x, 1]] <- round(sqrt(min(forest_cv_models[[x]]$mse)), digits = 4)
+      rmse_min_reg[[x, 2]] <- i
+      
+      
+      
+      forest_model_ite <- randomForest(Admit ~ . ,data = data, subset = train,
+                                   mtry = x, importance = TRUE, ntree = i)
+      
+      res_rdn = yhat - data$Admit[-train]
+      yhat <- predict(forest_model_ite, newdata = data[-train,])
+      test_RMSE_forest <- sqrt(mean(res_rdn^2))
+      
+      
+      
+      rmse_min_reg[[x, 3]] <- round(test_RMSE_forest, digits = 4)
     }
   }
 }
 
 
-test_MSE_forest = min(mse_cv_forest)
+
+
+
+
+par(mfrow = c(3,2))
+plot(sqrt(forest_cv_models[[1]]$mse),type = 'b', ylab = "RMSE", xlab = "number of trees", main = "Random forest with 1 regressor per split")
+abline(v = index_min[1], col = "blue", lty = "dashed")
+plot(sqrt(forest_cv_models[[2]]$mse),type = 'b', ylab = "RMSE", xlab = "number of trees", main = "Random forest with 2 regressor per split")
+abline(v = index_min[2], col = "blue", lty = "dashed")
+plot(sqrt(forest_cv_models[[3]]$mse),type = 'b', ylab = "RMSE", xlab = "number of trees", main = "Random forest with 3 regressor per split")
+abline(v = index_min[3], col = "blue", lty = "dashed")
+plot(sqrt(forest_cv_models[[4]]$mse),type = 'b', ylab = "RMSE", xlab = "number of trees", main = "Random forest with 4 regressor per split")
+abline(v = index_min[4], col = "blue", lty = "dashed")
+plot(sqrt(forest_cv_models[[5]]$mse),type = 'b', ylab = "RMSE", xlab = "number of trees", main = "Random forest with 5 regressor per split")
+abline(v = index_min[5], col = "blue", lty = "dashed")
+plot(sqrt(forest_cv_models[[6]]$mse),type = 'b', ylab = "RMSE", xlab = "number of trees", main = "Random forest with 6 regressor per split")
+abline(v = index_min[6], col = "blue", lty = "dashed")
+
+
+
+# best model validation
+forest_model <- randomForest(Admit ~ . ,data = data, subset = train,
+                             mtry = 2, importance = TRUE, ntree = 78)
+
+res_rdn = yhat - data$Admit[-train]
+yhat <- predict(forest_model, newdata = data[-train,])
+test_RMSE_forest <- round(sqrt(mean(res_rdn^2)), digits = 4) 
+
+forest_model
+importance(forest_model)
+
+
+
+
+# residuals
+bptest(forest_model) 
+shapiro.test(res_rdn)
+par(mfrow = c(1,4))
+hist(res_rdn,40,
+     xlab = "Residual",
+     main = "Distribuzione empirica dei residui") 
+
+plot(res_rdn, pch = "o", col = "blue" ,
+     ylab = "Residual", main = paste0("Residual plot - mean:",round(mean(res_rdn),digits = 4),
+                                      "- var:", round(var(res_rdn),digits = 4)))
+abline(c(0,0),c(0,length(res_rdn)), col= "red", lwd = 2)
+
+boxplot(res_rdn, ylab = "Residuals", main = "Outliers")$out
+
+qqnorm(res_rdn, main='Residuals')
+qqline(res_rdn)
+
 
 ################################################################################
-#########################################  Random forest
+#########################################  Boosting
 ################################################################################
 
 install.packages("gbm")
 library(gbm)
 set.seed(1)
 
-ntree = 5000; 
+
+
+ntree = 6000; 
+rmse_cv_boost <- rep(0,3)
+boosting_cv_models <- list()
+for(i in 1:3){
+  boost_model_ite <- gbm(Admit ~ . , data = data[train,], 
+                         distribution = "gaussian" , n.trees = ntree,
+                         interaction.depth = i, shrinkage = 0.001, verbose = F)
+  yhat_ite <- predict(boost_model_ite, newdata = data[-train,], n.trees = ntree)
+  rmse_cv_boost[i] <- round(sqrt(mean((yhat_ite - data$Admit[-train])^2)), digits = 5)
+  boosting_cv_models <- append(boosting_cv_models, list(boost_model_ite))
+}
+
+
+
+# plotting the 3 models using the same learning rate but different mumber of regressors
+# per split
+par(mfrow = c(1,1))
+plot(sqrt(boosting_cv_models[[1]]$train.error),  ylab = "train RMSE", xlab = "number of trees", main = "Boosting model with different deapths", col = "#5EC284", lwd=1)
+par(new=TRUE)
+plot(sqrt(boosting_cv_models[[2]]$train.error), axes=FALSE,  ann=FALSE, col = "#738DF9")
+par(new=TRUE)
+plot(sqrt(boosting_cv_models[[3]]$train.error),  axes=FALSE,  ann=FALSE, col = "#D28124")
+legend(5000, 0.12, legend=c("depth = 1", "depth = 2", "depth = 3"), fill =c("#5EC284", "#738DF9", "#D28124"))
+
+
+# min depth
+min_depth <- rep(0,1)
+for(i in 1:3){
+  if(rmse_cv_boost[i] == min(rmse_cv_boost)){
+    min_depth <- i
+  }
+}
+
+
+# validation
 boost_model <- gbm(Admit ~ . , data = data[train,], 
                    distribution = "gaussian" , n.trees = ntree,
-                   interaction.depth = 4, shrinkage = 0.01 , verbose = F)
-boost_model
-
+                   interaction.depth = min_depth, shrinkage = 0.001 , verbose = F)
 yhat <- predict(boost_model, newdata = data[-train,], n.trees = ntree)
-
-# test mse migliore dell'altro
-test_MSE_boost <- mean((yhat - data$Admit[-train])^2)
-
-
-mse_cv_boost <- matrix(nrow=3, ncol=40)
-leaning_rate = c(0.01, 0.1, 1)
-span_x = 1:40
+res_boostin = yhat - data$Admit[-train]
+test_RMSE_boost <- round(sqrt(mean((res_boostin)^2)), digits = 5)
 
 
-for(x in 1:3){
-  for(i in span_x){
-    ntree = (1000*i)
-    boost_model_ite <- gbm(Admit ~ . , data = data[train,], 
-                       distribution = "gaussian" , n.trees = ntree,
-                       interaction.depth = 4, shrinkage = leaning_rate[x] , verbose = F)
-    yhat_ite <- predict(boost_model_ite, newdata = data[-train,], n.trees = ntree)
-    mse_cv_boost[[x,i]] <- mean((yhat_ite - data$Admit[-train])^2)
-  }
-}
+# residuals
+shapiro.test(res_boostin)
+par(mfrow = c(1,4))
+hist(res_boostin,40,
+     xlab = "Residual",
+     main = "Distribuzione empirica dei residui") 
 
+plot(res_boostin, pch = "o", col = "blue" ,
+     ylab = "Residual", main = paste0("Residual plot - mean:",round(mean(res_boostin),digits = 4),
+                                      "- var:", round(var(res_boostin),digits = 4)))
+abline(c(0,0),c(0,length(res_boostin)), col= "red", lwd = 2)
 
-par(mfrow = c(1,3))
-plot(span_x*1000, mse_cv_boost[1,], xlab = "trees", ylab = "test mse", type = 'b')
-plot(span_x*1000, mse_cv_boost[2,], xlab = "trees", ylab = "test mse", type = 'b')
-plot(span_x*1000, mse_cv_boost[3,], xlab = "trees", ylab = "test mse", type = 'b')
+boxplot(res_boostin, ylab = "Residuals", main = "Outliers")$out
 
-
-nTrees_forest <- matrix(nrow=2, ncol=6)
-
-for(x in 1:6){
-  for(i in 1:100){
-    if(mse_cv_forest[i] == min(mse_cv_forest[x,])){
-      nTrees_forest[1,x] <- (i * 10)
-      nTrees_forest[2,x] <- mse_cv_forest[i]
-    }
-  }
-}
-
-
-test_MSE_boost = min(mse_cv_boost)
-
-
-
-################################################################################
-#########################################  Random forest
-################################################################################
-
-
-# fare crossvalidazione sul numero di tree -> scegliere il migliore
-# fare crossvalidazione sul numero di regressori da considerare ad ogni split
-
-
+qqnorm(res_boostin, main='Residuals')
+qqline(res_boostin)
 
